56934b7b20f5a6a74d2ad060bab9b9603b464830
Narayanan Ts <tsnarayanan7@gmail.com>
2017-04-04 21:24:36 +0530

    uploads.js file

---
+++ public/js/uploads.js
@@ -0,0 +1,30 @@
+$('.upload-btn').on('click',function(){
+	$('#upload-input').click();
+	$('.progress-bar').text('0%');
+	$('.progress-bar').width('0%');
+});
+
+$('#upload-input').on('change',function(){
+	var files = $(this).get(0).files;
+	if(files.length>0){
+		// One or more files selected, process the file upload
+	}
+	var formData = new formData();
+	// loop through all the selected files
+	for(var i=0;i<files.length;i++){
+		var file = files[i];
+		// add the files to formData object for the data payload
+      	formData.append('uploads[]', file, file.name);
+	}
+});
+
+$.ajax({
+	url:'/upload',
+	type:'POST',
+	data:formData,
+	processData: false,
+	contentType: false,
+	success:function(data){
+		console.log('upload successful');
+	}
+});
