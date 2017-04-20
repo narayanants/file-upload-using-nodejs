c44234e93cb140358d90cee1de25a078334addd3
Narayanan Ts <tsnarayanan7@gmail.com>
2017-04-20 18:40:12 +0530

    added uploads.js

--- public/js/uploads.js
+++ public/js/uploads.js
@@ -1,117 +1,63 @@
-$('.upload-btn').on('click',function(){
-	$('#upload-input').click();
-	$('.progressbar').text('0%');
-	$('.progressbar').width('100%');
+$('.upload-btn').on('click', function (){
+    $('#upload-input').click();
+    $('.progress-bar').text('0%');
+    $('.progress-bar').width('0%');
 });
 
-$('.upload-btn').on('change',function(){
-	var files = $(this).get(0).files;
-	if(files.length > 0){
-		if(files.length > 0){
-			// one or more file selected, process the selected files
-			//create a formData object that will be sent as a payload using AJAX
-			var formData = new FormData();
+$('#upload-input').on('change', function(){
 
-			//loop through the selected files
-			for(var i=0;i<files.length;i++){
-				var file = files[i];
+  var files = $(this).get(0).files;
 
-				//add the file to formData object for the data payload
-				formData.append('uploads[]',file,file.name);
-			}
-		}
-	}
+  if (files.length > 0){
+    // create a FormData object which will be sent as the data payload in the
+    // AJAX request
+    var formData = new FormData();
+
+    // loop through all the selected files and add them to the formData object
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+
+      // add the files to formData object for the data payload
+      formData.append('uploads[]', file, file.name);
+    }
+
+    $.ajax({
+      url: '/upload',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(data){
+          console.log('upload successful!\n' + data);
+      },
+      xhr: function() {
+        // create an XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+
+        // listen to the 'progress' event
+        xhr.upload.addEventListener('progress', function(evt) {
+
+          if (evt.lengthComputable) {
+            // calculate the percentage of upload completed
+            var percentComplete = evt.loaded / evt.total;
+            percentComplete = parseInt(percentComplete * 100);
+
+            // update the Bootstrap progress bar with the new percentage
+            $('.progress-bar').text(percentComplete + '%');
+            $('.progress-bar').width(percentComplete + '%');
+
+            // once the upload reaches 100%, set the progress bar text to done
+            if (percentComplete === 100) {
+              $('.progress-bar').html('Done');
+            }
+
+          }
+
+        }, false);
+
+        return xhr;
+      }
+    });
+
+  }
 });
-
-//AJAX request to send formData
-
-$.ajax({
-	url:'/upload',
-	type:'POST',
-	data:formData,
-	processData:false,
-	contentType:false,
-	success:function(){
-		console.log('upload successful!'+ data);
-	},
-	xhr:function(){
-		//create an XMLHTTPRequest
-		var xhr = new XMLHTTPRequest();
-
-		xhr.upload.addEventListener('progress',function(){
-			var percentComplete = evt.loaded / evt.total;
-			percentComplete = parseInt(percentComplete * 100);
-
-			//update the Bootstrap progress bar with the new percentage
-			$('.progress-bar').text(percentComplete + '%');
-			$('.progress-bar').width(percentComplete + '%');
-
-			//once the upload reaches 100%, set the progress bar text to done
-			if(percentComplete === 100){
-				$('.progress-bar').html('done');
-			}
-		},false);
-		return xhr;	
-	}
-}
-
-
-$.ajax({
-	url:'/upload',
-	type:'POST',
-	data:formData,
-	processData:false,
-	contentType:false,
-	success:function(){
-		console.log('upload successful!' + data);
-	},
-	xhr:function(){
-		//create an XMLHTTPRequest
-		var xhr = new XMLHTTPRequest();
-
-		xhr.upload.addEventListener('progress',function(){
-			var percentComplete = evt.loaded / evt.total;
-			percentComplete = parseInt(percentComplete * 100);
-
-			//update the BOotstrap progress bar with the new percentage
-			$('.progress-bar').text(percentComplete + "'%");
-			$('.progress-bar').width(percentComplete + "%");
-
-			//once the upload reaches 100%,set the progress bar text to done
-			if(percentComplete === 100){
-				$('.progress-bar').html('done');
-			}
-		},false);
-		return xhr;
-	}
-});
-
-
-$.ajax({
-	url:'/upload',
-	type:'POST',
-	data:formData,
-	processData:false,
-	contentType:false,
-	success:function(){
-		console.log('Upload successful');
-	},
-	xhr:function(){
-		//create an XMLHTTPRequest
-		var xhr = new XMLHTTPRequest();
-
-		//create an progress event
-		xhr.upload.addEventListener('progress',function(evt){
-
-			var percentComplete = evt.loaded / evt.total;
-			percentComplete = parseInt(percentComplete * 100);
-
-			//check upload progress if it reaches 100%
-			if(percentComplete === 100){
-				$('.progress-bar').html('done');
-			}
-		},false);
-		return xhr;
-	}
-});
-
